fc1a6af202116a43f4f817ab5ac735bdc6e54e01
Sushant Kumar Thakur <sushant.thakur40795@gmail.com>
2021-01-30 01:54:29 +0530

    final touches

--- src/tools/fileSearch.js
+++ src/tools/fileSearch.js
@@ -1,59 +1,6 @@
-// import * as fs from 'fs-web';
-
 import axios from "axios";
-// import { accessSync } from 'fs';
 
-// export const fileNames = (dir) => {
-//   const files = fs.readdir(dir)
-//   console.log(files);
-//   return fetch;
-// };
+export const fileList = () => axios.get("http://localhost:8080");
 
-export const fileList = () => {
-  return axios.get("http://localhost:8080");
-  // .then((response) => {
-  //   // console.log(response.data);
-  //   // response.data.forEach(element => {
-  //   // data2.push(element);
-  //   console.log(response);
-  //   return response.data;
-
-  //   // });
-  //   // console.log(data2);
-
-  //   // data = response.data;
-  //   // return response.data;
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  // });
-
-  // console.log(data2);
-  // return data2;
-};
-
-// console.log(data);
-
-// return data;
-// const e = fetch("http://localhost:8080", {
-//   "method": "GET"
-// }
-// )
-// console.log(e);
-// return e
-
-// const { fs } = require('filer');
-// export const fileNames = (dir) => {
-//   fs.readdir(dir, function (err, files) {
-//     if (err) throw err;
-
-//     console.log(__dirname);
-//     return files;
-//   });
-
-// };
-
-
-export const filedata = (datapoint) => {
-  return axios.get("http://localhost:8080/" + `${datapoint}`);
-}
+export const filedata = (datapoint) =>
+  axios.get("http://localhost:8080/" + `${datapoint}`);
